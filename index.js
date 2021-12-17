60ff8c3a0fe137d94506696b0e9edc25b033ac21
AkbarZul <akbar.zul73@gmail.com>
2021-12-17 09:57:24 +0700

    connect to port and handler /

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("hello world");
+const express = require('express');
+
+const app = express();
+
+const port = 8000;
+
+app.listen(port, () => {
+    console.log(`Server is running at port ${port}`);
+});
+
+app.get("/", (req, res) => {
+    res.send("Welcome to beLaundry Made By Akbar Zulfikar");
+});
+
