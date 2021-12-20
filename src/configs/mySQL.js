f3dea61c08f965d61cd8801284a48f82b8dcc5f0
AkbarZul <akbar.zul73@gmail.com>
2021-12-20 09:35:35 +0700

    env and multer

--- src/configs/mySQL.js
+++ src/configs/mySQL.js
@@ -1,10 +1,17 @@
 const mySql = require("mysql");
 
+const {HOST, DB, PASSWORD, USER} = process.env;
+
+// console.log(HOST)
+// console.log(DB);
+// console.log(PASSWORD);
+// console.log(USER);
+
 const db = mySql.createConnection({
-    host: "localhost",
-    user: "akbar",
-    password: "31maret",
-    database: "belaundry",
+    host: HOST,
+    user: USER,
+    password: PASSWORD,
+    database: DB,
   });
   
   db.connect((err) => {
